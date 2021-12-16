0a0698ac3e1ba2a2f5fa1252edfecd90bba08acd
Mario Gabriel PC <mario.contate.me@gmail.com>
2021-12-16 15:59:13 -0300

    Trabalhando a paina inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
+//import Erro404 from './img/Erro404.svg'
 import React from 'react';
-import Erro404 from './img/Erro404.svg'
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={Erro404} alt="Erro404" className='App-logo' />
-        <p>
-          <code>Fotos do James Web</code><br />
-          <code>Aqui vão aparecer as fotos do James Webb, previsto para lançamento dia 24/12/2021</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-      </header>
+      <h1>James Webb</h1>
     </div>
   );
 }
