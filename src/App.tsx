58c563d9aef8e427bec4661afaf69f7ec4cd9fda
Mario Gabriel PC <mario.contate.me@gmail.com>
2021-12-17 22:57:50 -0300

    cabecalho index e style home

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 //import Erro404 from './img/Erro404.svg'
 import React from "react";
+import "./reset.css";
 import "../src/style.css";
 
 function App() {
@@ -7,8 +8,8 @@ function App() {
     <div className="App">
       <div className="container">
         <div className="header">
-          <h1 className="tituloPrincipal">James Webb</h1>
-          <button>Fotos</button>
+            <h1 className="tituloPrincipal">JAMES WEBB</h1>
+            <button className="btn-link-nav">Fotos</button>
         </div>
       </div>
     </div>
