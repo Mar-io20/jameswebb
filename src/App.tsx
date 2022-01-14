e96d76912f995515220fc4e00e17c46de09eef0c
Mario Gabriel PC <mario.contate.me@gmail.com>
2022-01-14 07:55:02 -0300

    atualizando package.json  removendo ^

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,16 @@ import "../src/style.css";
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <div className="header">
-          <h1 className="tituloPrincipal">JAMES WEBB</h1>
-          <button className="btn-link-nav">Fotos</button>
-        </div>
-        <main className="container-conteudo">
-          <section className="materia-principal">
-            <span className="conteudo-texto">
-              James webb foi lançado, e está a caminho do ponto de lagrange 2. Durante essa viagem ele estara se preparando para começar sua operação. Enquanto isso veja como ele estava na na sala limpa do espaçoporto europeu. 
-            </span>
-            <img className="conteudo-img" src="https://blogs.nasa.gov/webb/wp-content/uploads/sites/326/2021/12/Webb-clean-room-FG-768x1023.jpg"alt="James Webb na sala limpa do espaçoporto europeu. Crédito: NASA / Chris Gunn" />
-          </section>
-        </main>
-      </div>
+      <section className="cabecalho">
+      <a href="#" className="link-logo">JamesWebb</a>
+      <ul className="nav-link">
+        <li>Inicio</li>
+        <li>Sobre</li>
+        <li>Serviços</li>
+        <li>Depoimentos</li>
+        <li>Contato</li>
+      </ul>
+      </section>
     </div>
   );
 }
